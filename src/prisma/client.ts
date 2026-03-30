7fdd3143487aa7ffac4b17ad2482a4b5dd7438d9
RobaChimdesa <robachimdesa123@gmail.com>
2026-03-30 08:38:55 +0300

    Refactor Prisma client setup to include connection pooling and error handling for DATABASE_URL

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -1,26 +1,25 @@
-// import { PrismaClient } from '@prisma/client';
-// import { PrismaPg } from '@prisma/adapter-pg';
-// import { Pool } from 'pg';
+// import { PrismaClient } from "@prisma/client";
 
-// const connectionString = process.env.DATABASE_URL!;
-
-// if (!connectionString) {
-//   throw new Error("DATABASE_URL is not defined in .env file");
-// }
-
-// // Create connection pool
-// const pool = new Pool({ 
-//   connectionString 
-// });
-
-// const adapter = new PrismaPg(pool);
-
-// const prisma = new PrismaClient({ adapter });
+// const prisma = new PrismaClient();
 
 // export default prisma;
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
-const prisma = new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error('DATABASE_URL is not defined in .env file');
+}
+
+const pool = new Pool({
+  connectionString,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
